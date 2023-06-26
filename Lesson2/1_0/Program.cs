3ff52979b52fd893a5df2f19c9ba4f6a352a5596
Murat <murat.sadyrbekov@gmail.com>
2023-06-27 00:47:50 +0600

    added seminar2 examples

---
+++ Lesson2/1_0/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+int num_gen = new Random().Next(100,1000);
+int result = TakeNum(num_gen);
+Console.WriteLine(result);
