fec45148c2b4d57ee63023d10a7fcedaf698b4ba
Murat <murat.sadyrbekov@gmail.com>
2023-06-29 02:08:50 +0600

    feat: add final text of HomeTask_3

--- Lesson1/HomeTask_3/Program.cs
+++ Lesson1/HomeTask_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число  = ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("проверка на четность ");
+if (A%2==0)
+{
+    Console.WriteLine($"число  = {A} -> да");
+}
+else 
+Console.WriteLine($"число  = {A} -> нет");
