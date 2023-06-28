a27510b8de1c26cb15199befa88ae3bc890566b7
Murat <murat.sadyrbekov@gmail.com>
2023-06-29 03:27:47 +0600

    feat: add final text to HomeTask_2

--- Lesson2/HomeTask_2/Program.cs
+++ Lesson2/HomeTask_2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Write("Введите число = ");
+int num = int.Parse(Console.ReadLine());
+if (num<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (num>999)
+{
+int result = (num/100)%10;
+ Console.WriteLine($"{num} -> {result}");
+}
+else
+{
+ int result = num%10;
+ Console.WriteLine($"{num} -> {result}");
+}
