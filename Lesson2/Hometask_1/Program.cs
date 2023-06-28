bc21d4199a0f43b1974ab907b6541dedd3825b35
Murat <murat.sadyrbekov@gmail.com>
2023-06-29 03:09:02 +0600

    feat: add final test to Lesson2_HomeTask_1

--- Lesson2/Hometask_1/Program.cs
+++ Lesson2/Hometask_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число = ");
+int num = int.Parse(Console.ReadLine()!);
+while (num>999|num<100)
+{
+      Console.WriteLine("Введите трехзначное число = ");
+      num = int.Parse(Console.ReadLine()!);
+}
+int second_digit = (num-(num-num%100))/10;
+Console.WriteLine($"{num} -> {second_digit}");
+
+
