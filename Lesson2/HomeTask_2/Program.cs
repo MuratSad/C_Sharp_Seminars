956552b35adc48730fc60d5852397aa39762cddf
Murat <murat.sadyrbekov@gmail.com>
2023-06-30 13:21:19 +0600

    feat: add final code to HomeTask_2

--- Lesson2/HomeTask_2/Program.cs
+++ Lesson2/HomeTask_2/Program.cs
@@ -2,19 +2,29 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
-Console.Write("Введите число в диапазоне от 0 до 100000 = ");
+Console.Write("Введите число в диапазоне от 0 до 99999 = ");
 int num = int.Parse(Console.ReadLine());
-if (num<100)
+while (num < 0 | num > 99999)
+{
+    Console.WriteLine($"число {num} не входит в заданный диапазон");
+    num = int.Parse(Console.ReadLine());
+}
+if (num < 100)
 {
     Console.WriteLine("Третьей цифры нет");
 }
-else if (num>999)
+else if (num>100&&num<1000)
 {
-int result = (num/100)%10;
- Console.WriteLine($"{num} -> {result}");
+    int result = num % 10;
+    Console.WriteLine($"{num} -> {result}");
+}
+else if (num > 999 && num < 10000)
+{
+    int result = (num / 10) % 10;
+    Console.WriteLine($"{num} -> {result}");
 }
 else
 {
- int result = num%10;
- Console.WriteLine($"{num} -> {result}");
+    int result = (num / 100) % 10;
+    Console.WriteLine($"{num} -> {result}");
 }
