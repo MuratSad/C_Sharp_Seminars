fdfcb7dbd6ba214fed363f717a3b7bc41b7459fc
Murat <murat.sadyrbekov@gmail.com>
2023-07-02 00:34:52 +0600

    add final code to lesson 3 Hometask2

--- Lesson3/HomeTask2/Program.cs
+++ Lesson3/HomeTask2/Program.cs
@@ -3,11 +3,6 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.WriteLine("Введите координаты двух точек X,Y,Z");
-
-double DistanceBetweenTwoPoints(int x1, int y1, int z1, int x2, int y2, int z2);
-{
-    return Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
-}
 Console.WriteLine("Введите x1=");
 int x1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите y1=");
@@ -20,5 +15,5 @@ Console.WriteLine("Введите y2=");
 int y2 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите z2=");
 int z2 = int.Parse(Console.ReadLine()!);
-double result = DistanceBetweenTwoPoints(x1,y1,z1,x2,y2,z2);
-Console.WriteLine(result);
+double result = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+Console.WriteLine($"Расстояние между двумя точками = " + result);
