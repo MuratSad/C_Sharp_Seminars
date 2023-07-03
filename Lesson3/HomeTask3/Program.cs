4d095c24b780e6dd9cd4fecf242beccb9cf0ef43
Murat <murat.sadyrbekov@gmail.com>
2023-07-03 22:07:24 +0600

    feat: add final code to HomeTask3

---
+++ Lesson3/HomeTask3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+void QUB (int X,int y=1)
+{
+  while (y<=X)
+  {
+    int result=y*y*y;
+    Console.Write(","+result);
+    y+=1;
+  }
+}
+QUB(N);
