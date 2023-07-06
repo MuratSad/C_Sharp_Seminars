12ce5a23c085dff7d9ec4e44dd9ce57d322ed424
Murat <murat.sadyrbekov@gmail.com>
2023-07-06 21:09:41 +0600

    feat: add final code to Lesson4>HomeTask1

--- Lesson4/HomeTask1/Program.cs
+++ Lesson4/HomeTask1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Qub (int A,int B)
+{
+    int result = A;
+    for (int i=1;i<B;i++)
+    {
+        result=result*A;
+    }
+    return result;
+}
+Console.WriteLine("Введите натуральное число A");
+int A =int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите натуральное число B");
+int B =int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{A}, {B} -> {Qub (A,B)}");
