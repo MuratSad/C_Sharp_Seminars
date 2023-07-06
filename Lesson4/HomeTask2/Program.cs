005ce5fa3d05646a725f52374e23c473c18b6b36
Murat <murat.sadyrbekov@gmail.com>
2023-07-06 23:37:52 +0600

    feat:add final code to HomeTask2 Lesson4

--- Lesson4/HomeTask2/Program.cs
+++ Lesson4/HomeTask2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.WriteLine("Введите натуральное число Num");
+int Num = int.Parse(Console.ReadLine()!);
+int GetLenghtOfInt(int A)
+{
+    int Count = 0;
+    int x = A;
+    while (x != 0)
+    {
+        x = x / 10;
+        Count++;
+    }
+
+    return Count;
+}
+int SumOfDigits (int B)
+{
+    int result = 0;
+    for (int j=10;B!=0;)
+    {
+        result=result+B%j;
+        B=B/10;
+    }
+    
+    return result;
+}
+Console.Write(SumOfDigits(Num));
+
