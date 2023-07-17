70a8c7668d1e77ae7c0c319da69a2984f679f733
Murat <murat.sadyrbekov@gmail.com>
2023-07-17 20:35:17 +0600

    feat:add final code to Hometask 1 and HomeTask2

--- Lesson6/HomeTask1/Program.cs
+++ Lesson6/HomeTask1/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа, по окончании введите списка чисел введите просто пробел");
+
+
+string strNumber = string.Empty;
+void PrintMassive(double[] Array) // печать масссива вещественных чисел на экран
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"{Array[i]}|");
+    }
+    Console.WriteLine("");
+}
+double[] CreateDoubleTypeMassive(int Size = 0, double MinValue = 0, double MaxValue = 0) //Функция для создание массива вещественных чисел (размерностью Size,минимальным значением рандомайзера MinValue,и максимальным значением MaxValue)
+
+{
+    Random rand = new Random();
+    if (Size == 0)
+    {
+        while (Size <= 0)
+        {
+            Console.Write("Введите размерность массива = ");
+            Size = int.Parse(Console.ReadLine()!);
+        }
+        while (MinValue == 0)
+        {
+            Console.Write("Введите минимальное значение диапазона чисел=");
+            MinValue = int.Parse(Console.ReadLine()!);
+        }
+        while (MaxValue == 0)
+        {
+            Console.Write("Введите максимальное значение диапазона чисел=");
+            MaxValue = int.Parse(Console.ReadLine()!);
+        }
+    }
+    double[] arr = new double[Size];
+    for (int i = 0; i < Size; i++)
+    {
+        arr[i] = Math.Round(rand.NextDouble() * (MaxValue - MinValue) + MinValue, 2);
+    }
+    return arr;
+}
+void ResultNumbersMoreThanZero(double[] Array) //Метод для подсчета значений выше 0
+{
+    int size = Array.Length;
+    int count=0;
+     for (int i = 0; i < size; i++)
+    {
+        if (Array[i] > 0) count ++;
+        
+    }
+    Console.WriteLine($"Количество значений больше 0  = {count}");
+}
+double[] array = CreateDoubleTypeMassive(20,-10,30); //задаем массив размером 40, от -10, до 30)
+ResultNumbersMoreThanZero(array);
+PrintMassive(array);
--- Lesson6/HomeTask2/Program.cs
+++ Lesson6/HomeTask2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//            заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//            значения b1, k1, b2 и k2 задаются пользователем.
+//            b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите целове число b1");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите целове число k1");
+int k1 = int.Parse(Console.ReadLine()!); ;
+Console.WriteLine("Введите целове число b2");
+int b2 = int.Parse(Console.ReadLine()!); ;
+Console.WriteLine("Введите целове число k2");
+int k2 = int.Parse(Console.ReadLine()!); ;
+
+void PointOfCrossLines(int b1, int k1, int b2, int k2)
+{
+    double x = 0;
+    double y = 0;
+    if (k1 - k2 != 0)
+    {
+        if(k1-k2<0)
+        {
+        x = (b2 - b1)/Math.Abs(k1 - k2);
+        }
+        else x = (b2 - b1) / (k1 - k2);
+    }
+    else Console.WriteLine("k1=k2, происходит деление на ноль");
+    y = k1 * x + b1;
+    Console.WriteLine($"b1 = {b1}, k1 = {k1},  b2 = {b2}, k2 = {k2}, => ({x} , {y})");
+}
+PointOfCrossLines(b1, k1, b2, k2);
