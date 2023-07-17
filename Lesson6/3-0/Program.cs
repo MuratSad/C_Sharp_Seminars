9ca5d75df7e502d662c5b51e8b2201689c8e1656
Murat <murat.sadyrbekov@gmail.com>
2023-07-17 13:00:57 +0600

    feat:add new folder HomeTask1,2,3

---
+++ Lesson6/3-0/Program.cs
@@ -0,0 +1,19 @@
+﻿// 2. Напишите программу, которая будет
+//    преобразовывать десятичное число в двоичное.
+
+// 2. Напишите программу, которая будет
+//    преобразовывать десятичное число в двоичное.
+
+
+string BinaryNumber(int num)
+{
+    string number = string.Empty;
+
+    for (; num != 0; num /= 2)
+    {
+        number = num % 2 + number;
+    }
+    return number;
+}
+
+System.Console.WriteLine(BinaryNumber(12));
