f29e34544b198507422b920d8d7676c89fa347df
Murat <murat.sadyrbekov@gmail.com>
2023-07-21 01:05:45 +0600

    add Lesson 9 Examples

---
+++ Lesson9/4-0/Program.cs
@@ -0,0 +1,15 @@
+﻿// 4. Напишите программу, которая на вход принимает два числа A и B,
+//    и возводит число А в целую степень B с помощью рекурсии.
+
+int ReCursion(int A, int B)
+{
+    if (B == 0) return 1;
+    ReCursion(A, B - 1);
+
+    return ReCursion(A, B - 1) * A;
+}
+Console.WriteLine("Введите целое число A");
+int A=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите целое число B");
+int B=int.Parse(Console.ReadLine()!);
+Console.Write($"Результат возведения числа А={A} в степень B={B} => {ReCursion(A, B)}");
