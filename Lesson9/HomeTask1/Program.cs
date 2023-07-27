5cff56c2ab18b9387c7270247fdfdd8150305a91
Murat <murat.sadyrbekov@gmail.com>
2023-07-27 14:02:57 +0600

    feat:add final code to HomeTask1 Leson9

--- Lesson9/HomeTask1/Program.cs
+++ Lesson9/HomeTask1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+
+void PrintNumber(int num1,int num2)
+{
+    if (num1 % 2 == 0)
+    {
+        Console.Write($"{num1} ");
+    }
+    if (num2==num1) return;
+    num1=num1+1;
+    PrintNumber(num1,num2);
+    
+}
+Console.WriteLine("введите целое число M");
+int m=int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите целое число N");
+int n=int.Parse(Console.ReadLine()!);
+
+PrintNumber(m,n);
--- Lesson9/HomeTask2/Program.cs
+++ Lesson9/HomeTask2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int PrintSumOfNumbers(int num1,int num2,int sum=0)
+{
+     if (num1 % 2 == 0)
+    {
+        sum=sum+num1;
+    }
+    if (num2==num1) return sum;
+    num1=num1+1;
+    PrintSumOfNumbers(num1,num2,sum);
+    return sum;
+}
+
+
+Console.WriteLine("введите целое число M");
+int m=int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите целое число N");
+int n=int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintSumOfNumbers(m,n));
