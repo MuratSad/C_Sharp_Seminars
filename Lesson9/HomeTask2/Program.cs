4f88efb062ab8be13512c1d8b6584a6f0a525b39
Murat <murat.sadyrbekov@gmail.com>
2023-07-27 14:33:09 +0600

    feat:add final code to HomeTask2 Lesson9

--- Lesson9/HomeTask2/Program.cs
+++ Lesson9/HomeTask2/Program.cs
@@ -4,22 +4,19 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30
 
-int PrintSumOfNumbers(int num1,int num2,int sum=0)
+int PrintSumOfNaturalNumbers(int num1, int num2)
 {
-     if (num1 % 2 == 0)
+    if (num1 > num2)
     {
-        sum=sum+num1;
+        return 0;
     }
-    if (num2==num1) return sum;
-    num1=num1+1;
-    PrintSumOfNumbers(num1,num2,sum);
-    return sum;
+    return num1+PrintSumOfNaturalNumbers(num1+1,num2);
 }
 
 
 Console.WriteLine("введите целое число M");
-int m=int.Parse(Console.ReadLine()!);
+int m = int.Parse(Console.ReadLine()!);
 Console.WriteLine("введите целое число N");
-int n=int.Parse(Console.ReadLine()!);
+int n = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine(PrintSumOfNumbers(m,n));
+Console.WriteLine(PrintSumOfNaturalNumbers(m, n));
